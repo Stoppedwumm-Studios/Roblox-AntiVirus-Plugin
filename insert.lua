5ca8bee3b5feb92e639c9b0b8229fb9c9f8e1d02
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-07-02 16:19:52 +0200

    Update insert.lua

--- insert.lua
+++ insert.lua
@@ -2,3 +2,4 @@ local Event = Instance.new("RemoteEvent")
 local LocalScript = Instance.new("LocalScript")
 local http = game:GetService("HttpService")
 LocalScript.Source = http:GetAsync("https://raw.githubusercontent.com/Stoppedwumm-Studios/Roblox-AntiVirus-Plugin/main/consolelogger.lua")
+LocalScript.Parent = game.StarterPlayer.StarterPlayerScripts

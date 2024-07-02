31fbfaf2d8ea6ef8c0221e37094c616bb344dc29
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-07-02 15:23:15 +0200

    Update plugin-main.lua

--- plugin-main.lua
+++ plugin-main.lua
@@ -1,35 +1,57 @@
 -- Plugin metadata
-local toolbar = plugin:CreateToolbar("Security Tools")
-local button = toolbar:CreateButton("Scan for Loadstring", "Scans all scripts for loadstring usage", "rbxassetid://4458901886")
+local toolbar = plugin:CreateToolbar("AntiVirus")
+local scanbutton = toolbar:CreateButton("Scan", "Scans globally for :Kick/loadstring", "rbxassetid://4458901886")
+local insertButton = toolbar:CreateButton("Insert Console Opener", "Insert an console open checker", "rbxassetid://4458901886")
 
 -- Function to scan scripts
 local function scanScripts()
 	-- Get all the scripts in the game
 	local scripts = game:GetDescendants()
-	local foundScripts = {}
+	local lsScripts = {}
+	local kScripts = {}
+	local hScripts = {}
+	local mainCounter = 0
 
 	-- Scan each script
 	for _, obj in ipairs(scripts) do
 		if obj:IsA("LuaSourceContainer") then
 			local scriptSource = obj.Source
 			if scriptSource:find("loadstring") or scriptSource:find(":Kick") then
-				table.insert(foundScripts, obj:GetFullName())
+				table.insert(lsScripts, obj:GetFullName())
+				mainCounter += 1
+			end
+			if scriptSource:find(":Kick") or scriptSource:Find(":kick") then
+				table.insert(kScripts, obj:GetFullName())
+				mainCounter += 1
+			end
+			if scriptSource:find("HttpService") then
+				table.insert(kScripts, obj:GetFullName())
+				mainCounter += 1
 			end
 		end
 	end
 
 	-- Notify the developer
-	if #foundScripts > 0 then
-		local message = "Warning: Found loadstring/kick usage in the following scripts:\n"
-		for _, scriptName in ipairs(foundScripts) do
+	if mainCounter > 0 then
+		local message = "Warning: Found loadstring usage in the following scripts:\n"
+		for _, scriptName in ipairs(lsScripts) do
 			message = message .. scriptName .. "\n"
 		end
-		print(message)
+		warn(message)
+		local message = "Warning: Found kick usage in the following scripts:\n"
+		for _, scriptName in ipairs(kScripts) do
+			message = message .. scriptName .. "\n"
+		end
+		warn(message)
+		local message = "Warning: Found HttpService usage in the following scripts:\n"
+		for _, scriptName in ipairs(hScripts) do
+			message = message .. scriptName .. "\n"
+		end
 		warn(message)
 	else
-		print("No loadstring usage found.")
+		print("No suspicous scripts found")
 	end
 end
 
 -- Bind the function to the button click
-button.Click:Connect(scanScripts)
+scanbutton.Click:Connect(scanScripts)
